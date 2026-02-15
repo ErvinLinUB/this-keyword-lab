b9313d58c331d373a943d0375450ddf31cdaf7b6
Ervin Lin <2024161139@ub.edu.bz>
2026-02-15 16:23:38 -0600

    Complete Exercise 1: Multi-Button Highlight.

---
+++ exercise1.js
@@ -0,0 +1,17 @@
+// Exercise 1: The Multi-Button Highlight
+
+const buttons = document.querySelectorAll('.color-btn');
+
+// 1. Define a function called changeColor
+// Inside it, use 'this' to change the background to 'yellow'
+
+function changeColor() {
+    this.style.backgroundColor = 'yellow';
+}
+
+// 2. Loop through buttons with forEach
+// Attach changeColor as the click handler
+
+buttons.forEach(button => {
+    button.addEventListener('click', changeColor)
+});
